275ea3e83f50f7c8db81e5770b554c9f4b89c975
gazhur94 <gazhur94@gmail.com>
2019-03-24 22:31:12 +0200

    added tests

--- app/Result.php
+++ app/Result.php
@@ -15,9 +15,9 @@ class Result
         try {
             $this->pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
             $res = $this->pdo->query("SELECT count(id) as count FROM storage");
-            if (intval($res->fetch(PDO::FETCH_ASSOC)['count']) > 2) {
-                $this->errors[] = "Storage is full";
-                $this->response();
+            if (intval($res->fetch(PDO::FETCH_ASSOC)['count']) > 1024) {
+                $this->errors[] = "storage is full";
+                return $this->response();
                 die();
             }
         } catch (PDOException $e) {
@@ -32,7 +32,7 @@ class Result
         $result = $exists->fetch(PDO::FETCH_ASSOC);
 
         if(!$result) {
-            $this->errors[] =  "Key not exists: ".$key;
+            $this->errors[] =  "key not exists: ".$key;
         } else {
             $this->success = ["value" => $result['value_']];
         }
@@ -44,18 +44,18 @@ class Result
     {
         $exists = $this->pdo->query("SELECT key_ FROM storage WHERE key_ = '$key'");
         if($exists->fetch()) {
-            $this->errors[] = "Dublicate key: ".$key;
+            $this->errors[] = "dublicate key: ".$key;
         }
 
         if (strlen($key)  >= 16) {
-            $this->errors[] = "Too long key length";
+            $this->errors[] = "key too long";
         }
 
         $res = $this->pdo->prepare("INSERT INTO storage (key_, value_) VALUES ('$key', '$value')");
         if ($res->execute()) {
             $this->success = ["status" => "success"];
         } else {
-            $this->errors[] = "Problems with database connection";
+            $this->errors[] = "problems with database connection";
         }
         return $this->response();
     }
@@ -64,7 +64,7 @@ class Result
     {
         $exists = $this->pdo->query("SELECT key_ FROM storage WHERE key_ = '$key'");
         if(!$exists->fetch()) {
-            $this->errors[] =  "Key not exists: ".$key;
+            $this->errors[] =  "key not exists: ".$key;
         }
 
         $res = $this->pdo->prepare("DELETE FROM storage WHERE key_ = '$key'");
@@ -73,19 +73,19 @@ class Result
         if ($res->execute()) {
             $this->success = ["status" => "success"];
         } else {
-            $this->errors[] = "Problems with database connection";
+            $this->errors[] = "problems with database connection";
         }
 
         return $this->response();
     }
 
-    private function response()
+    public function response()
     {
         if (count($this->errors) > 0) {
             http_response_code(400);
-            echo json_encode(["error" => $this->errors[0]]);
+            return json_encode(["error" => $this->errors[0]]);
         } else {
-            echo json_encode($this->success);
+            return json_encode($this->success);
         }
     }
 }
--- tests/unit/ResultTest.php
+++ tests/unit/ResultTest.php
@@ -2,11 +2,56 @@
 
 class ResultTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetMethod()
-    {
-        $result = new \App\Result;
+    protected $result;
+    protected $newKey = 'some_new_key';
+    protected $newValue = 'some_new_value';
 
+    public function setUp()
+    {
+        $this->result = new \App\Result;
     }
+
+    /** @test */
+    public function that_return_error_about_non_existent_key_in_get()
+    {
+        $key = 'some_non_existend_key';
+        $this->assertEquals($this->result->getValueByKey($key), json_encode(["error" => "key not exists: ".$key]));
+    }
+
+    /** @test */
+    public function that_return_error_about_non_existent_key_in_delete()
+    {
+        $key = 'some_non_existend_key';
+        $this->assertEquals($this->result->deleteValueByKey($key), json_encode(["error" => "key not exists: ".$key]));
+    }
+
+    /** @test */
+    public function that_return_error_about_long_key_in_set_new_value()
+    {
+        $key = 'my_very_long_key_10000000';
+        $value = 'my_value_for_long_key';
+        $this->assertEquals($this->result->setNewValue($key, $value), json_encode(["error" => "key too long"]));
+    }
+
+    /** @test */
+    public function set_new_value()
+    {
+        $this->assertEquals($this->result->setNewValue($this->newKey, $this->newValue), json_encode(["status" => "success"]));
+    }
+
+    /** @test */
+    public function get_new_value()
+    {
+        $this->assertEquals($this->result->getValueByKey($this->newKey), json_encode(["value" => $this->newValue]));
+    }
+
+    /** @test */
+    public function delete_new_value()
+    {
+        $this->assertEquals($this->result->deleteValueByKey($this->newKey), json_encode(["status" => "success"]));
+    }
+
+
 }
 
 
